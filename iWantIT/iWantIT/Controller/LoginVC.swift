5de0701a603518a9d845d380aae8abfa980ec5f6
Supriyo Dey <supriyodeyiosdev@gmail.com>
2024-05-23 16:00:50 +0530

    home ui only

--- iWantIT/iWantIT/Controller/LoginVC.swift
+++ iWantIT/iWantIT/Controller/LoginVC.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    @IBOutlet weak var lblAppTitle: UILabel!
+    @IBOutlet weak var lblLogin: UILabel!
+    @IBOutlet weak var lblLoginSubText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initialUiSetup()
     }
     
+    func initialUiSetup() {
+        lblAppTitle.font = UIFont(name: "Baloo Da", size: 34)
+        lblLogin.font = UIFont(name: "Inter Bold", size: 22)
+        lblLoginSubText.font = UIFont(name: "Inter Regular", size: 17)
+    }
 }
