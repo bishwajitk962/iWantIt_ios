974365c5c8364ed0dd5215ac06f20b649c771316
Supriyo Dey <supriyodeyiosdev@gmail.com>
2024-05-24 15:37:20 +0530

    IQKeyboardManagerSwift Pod Installed, Signup Page UI

---
+++ iWantIT/Pods/Target Support Files/Pods-iWantIT/Pods-iWantIT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iWantIT : NSObject
+@end
+@implementation PodsDummy_Pods_iWantIT
+@end
--- iWantIT/iWantIT/Controller/LoginVC.swift
+++ iWantIT/iWantIT/Controller/LoginVC.swift
@@ -11,16 +11,37 @@ class LoginVC: UIViewController {
     @IBOutlet weak var lblAppTitle: UILabel!
     @IBOutlet weak var lblLogin: UILabel!
     @IBOutlet weak var lblLoginSubText: UILabel!
+    @IBOutlet weak var constImgHeight: NSLayoutConstraint!
+    @IBOutlet weak var constImgAspectRation: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         initialUiSetup()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    @IBAction func loginBtnAction(_ sender: UIButton) {
+        
+    }
+    @IBAction func signUpBtnAction(_ sender: UIButton) {
+        goToSignup()
+    }
+}
+
+//MARK: - all functions
+extension LoginVC {
     func initialUiSetup() {
         lblAppTitle.font = UIFont(name: "Baloo Da", size: 34)
-        lblLogin.font = UIFont(name: "Inter Bold", size: 22)
+        lblLogin.font = UIFont(name: "Inter Medium", size: 22)
         lblLoginSubText.font = UIFont(name: "Inter Regular", size: 17)
+    }
+    
+    func goToSignup() {
+        if let signUpVc = self.storyboard?.instantiateViewController(withIdentifier: "SignUpVC") as? SignUpVC {
+            self.navigationController?.pushViewController(signUpVc, animated: true)
+        }
     }
 }
