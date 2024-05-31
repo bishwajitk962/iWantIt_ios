9d7f7f9c2672c3dc98607b99efb91405fb1b188b
Supriyo Dey <supriyodeyiosdev@gmail.com>
2024-05-31 11:12:01 +0530

    logout popup

--- iWantIT/iWantIT/Controller/SettingsVC.swift
+++ iWantIT/iWantIT/Controller/SettingsVC.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class SettingsVC: UIViewController {
-
+    @IBOutlet weak var vwAlertPopup: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initialUISetup()
     }
     @IBAction func backBtnAction(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func logoutBtnAction(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.2) {
+            self.vwAlertPopup.alpha = 1
+            self.vwAlertPopup.isHidden = false
+        } completion: { _ in
+            self.vwAlertPopup.isHidden = false
+        }
+    }
+    @IBAction func neverMindAlertBtnAction(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.2) {
+            self.vwAlertPopup.alpha = 0
+        } completion: { _ in
+            self.vwAlertPopup.isHidden = true
+        }
+    }
+    @IBAction func confirmAlertBtnAction(_ sender: UIButton) {
+        vwAlertPopup.isHidden = true
+        vwAlertPopup.alpha = 0
+        
+        if let viewControllers = navigationController?.viewControllers {
+            for viewController in viewControllers {
+                if let loginVc = viewController as? LoginVC {
+                    navigationController?.popToViewController(loginVc, animated: true)
+                    break
+                }
+            }
+        }
+
+    }
+}
+
+extension SettingsVC {
+    func initialUISetup() {
+        vwAlertPopup.isHidden = true
+        vwAlertPopup.alpha = 0
     }
 }
